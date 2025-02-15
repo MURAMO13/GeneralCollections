bd1d5a64c0a57b7df15cb564a36334af218a9a9c
muramo13 <monbon08494@gmail.com>
2025-02-16 00:25:25 +0500

    Добавьте файлы проекта.

---
+++ Lists.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandBox
+{
+    internal class Lists
+    {
+        internal static void ListSW(string[] strings) 
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            List<string> list = new List<string>();
+            foreach (string s in strings)
+            {
+                list.Add(s);
+            }
+            sw.Stop();
+
+            Console.WriteLine($"Time elapsed for List: {sw.ElapsedMilliseconds} ms");
+        }
+
+        internal static void LinkedListSW(string[] strings)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            LinkedList<string> linkedList = new LinkedList<string>();
+            foreach (string s in strings)
+            {
+                linkedList.AddLast(s);
+            }
+            sw.Stop();
+            Console.WriteLine($"Time elapsed for LinkedList: {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using SandBox;
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    private readonly static string _urltoTxt = "https://drive.usercontent.google.com/u/0/uc?id=1jg43arS4KIUwO0-kao_ga7PWIPWBOXxo&export=download";
+    public static async Task Main()
+    {
+        string[] txtForFirstThread = await GetTextFromUrl(_urltoTxt);
+       
+        if (txtForFirstThread != null)
+        {
+
+            Lists.ListSW(txtForFirstThread);
+            Lists.LinkedListSW(txtForFirstThread);
+
+
+            //В данном случае я пытался запустить два потока,
+            //но результаты были разными.
+            //Поэтому я решил оставить только один поток.Буду признателен,
+            //если вы сможете объяснить мне причину такого поведения.
+
+            //string[] txtForSecondThread = (string[])txtForFirstThread.Clone();
+
+            //Thread threadFirst = new Thread(() => Lists.ListSW(txtForFirstThread));
+            //threadFirst.Start();
+
+            //Thread threadSecond = new Thread(() => Lists.LinkedListSW(txtForSecondThread));
+            //threadSecond.Start();
+        }
+
+        Console.ReadLine();
+    }
+
+    private static async Task<string[]> GetTextFromUrl(string url)
+    {
+        try
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string txtTemp = await httpClient.GetStringAsync(url);
+                if (txtTemp != null)
+                {
+                    return txtTemp.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return null;
+        }
+    }
+}
